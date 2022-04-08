7dcf38341bebec12dca265c5c55d77e9c1f094d2
Mohammed Ahnaf Abid <73640125+MohammedAhnafAbid1931897642@users.noreply.github.com>
2022-04-08 09:27:57 +0600

    Good

---
+++ index.php
@@ -0,0 +1,416 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title> Focus Furniture & Interior </title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
+.w3-row-padding img {margin-bottom: 12px}
+/* Set the width of the sidebar to 120px */
+.w3-sidebar {width: 120px;background: #222;}
+/* Add a left margin to the "page content" that matches the width of the sidebar (120px) */
+#main {margin-left: 120px}
+/* Remove margins from "page content" on small screens */
+@media only screen and (max-width: 600px) {#main {margin-left: 0}}
+</style>
+</head>
+<body class="w3-black">
+
+<!-- Icon Bar (Sidebar - hidden on small screens) -->
+<nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
+  <!-- Avatar image in top left corner -->
+  
+  <a href="#" class="w3-bar-item w3-button w3-padding-large w3-black">
+    <i class="fa fa-home w3-xxlarge"></i>
+    <p>HOME</p>
+  </a>
+  <a href="#biography" class="w3-bar-item w3-button w3-padding-large w3-black">
+    <i class="fa fa-user w3-xxlarge"></i>
+    <p>BIOGRAPHY</p>
+  </a>
+  <a href="#portfolio" class="w3-bar-item w3-button w3-padding-large w3-black">
+    <i class="fa fa-eye w3-xxlarge"></i>
+    <p>PORTFOLIO</p>
+  </a>
+  <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-black">
+    <i class="fa fa-envelope w3-xxlarge"></i>
+    <p>CONTACT</p>
+  </a>
+
+  <a href="#about" class="w3-bar-item w3-button w3-padding-large w3-black">
+    <i class="fa fa-envelope w3-xxlarge"></i>
+    <p>ABOUT</p>
+  </a>
+
+  <a href="#reviews" class="w3-bar-item w3-button w3-padding-large w3-black">
+    <i class="fa fa-home w3-xxlarge"></i>
+    <p>REVIEWS</p>
+  </a>
+</nav>
+
+<!-- Navbar on small screens (Hidden on medium and large screens) -->
+<div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
+  <div class="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
+    <a href="#" class="w3-bar-item w3-button" style="width:25% !important">HOME</a>
+    <a href="#biography" class="w3-bar-item w3-button" style="width:25% !important">BIO</a>
+    <a href="#portfolio" class="w3-bar-item w3-button" style="width:25% !important">PORTFOLIO</a>
+    <a href="#contact" class="w3-bar-item w3-button" style="width:25% !important">CONTACT</a>
+    <a href="#about" class="w3-bar-item w3-button" style="width:25% !important">ABOUT</a>
+    <a href="#reviews" class="w3-bar-item w3-button" style="width:25% !important">REVIEWS</a>
+  </div>
+</div>
+
+<!-- Page Content -->
+<div class="w3-padding-large" id="main">
+  <!-- Header/Home -->
+  <header class="w3-container w3-padding-32 w3-center w3-black" id="home">
+    <br>
+    <br>
+    <br>
+  <img src="images/logo.jpg" alt="boy" class="w3-image" width="400" height="400">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <h1 class="w3-jumbo"><span class="w3-hide-small"></span> I'm <br> Mohammed Shamsuddin</h1>
+    <h3 class="w3-padding-16 w3-text-light-grey">Interior Designer & Furniture Manufacturer</h3>
+    <h2 class="w3-padding-16 w3-text-light-grey">At Focus</h2>
+    <img src="images/f1-1.jpg" alt="boy" class="w3-image" width="400" height="400">
+  </header>
+
+    <!-- Stars and Review count -->
+    <div class="w3-black w3-padding-large w3-padding-32 w3-margin-top" id="bio">
+    <h3 class="w3-center">Ratings Preview</h3>
+    <hr>
+    <fieldset class="w3-center"> </fieldset>
+   
+
+  </div>
+
+  <!-- Biography Section -->
+  <div class="w3-content w3-justify w3-text-grey w3-padding-64" id="biography">
+    <h2 class="w3-text-light-grey">My Biography</h2>
+    <hr style="width:200px" class="w3-opacity">
+    <p class="w3-black" > My name is Md Shamsuddin, son of late Totamia, my father was the first furniture supplier of Bangladesh Television and Bangladesh Airport at Tejgaon right after independence in 1971. Late Totamia was also the first packaging contractor of the US Embassy, Dhaka, and Cane & Rattan furniture manufacturer from Sylhet. I worked with my father in the family business as his apprentice from 1985 to 1993. His business operations were mostly involved with foreign delegations.
+In 1994, I proposed to my family to start my own furniture business by getting funding from financial institutions to which I received positive support. I then proceeded to get the financing.
+I have been working in furniture manufacturing and interior design since 1985. I have numerous achievements in the Bangladesh Furniture Industry and have successfully done trial runs on exporting products to European (Ellos, Sweden) and American (Paul Robert Furniture, USA) buyers. 
+    </p>
+
+    
+  
+  <!-- Portfolio Section -->
+  <div class="w3-padding-64 w3-content" id="portfolio">
+    <h2 class="w3-text-light-grey">My Portfolio</h2>
+    <hr style="width:200px" class="w3-opacity">
+
+    <!-- Grid for photos -->
+    <div class="w3-row-padding" style="margin:0 -16px">
+      <div class="w3-half">
+        <img src="images/f1-2.jpg" style="width:100%">
+        <img src="images/f1-3.jpg" style="width:100%">
+        <img src="images/f1-4.jpg" style="width:100%">
+        <img src="images/f1-5.jpg" style="width:100%">
+        <img src="images/f1-6.jpg" style="width:100%">
+        <img src="images/f1-12.jpg" style="width:100%">
+
+      </div>
+
+      <div class="w3-half">
+      <img src="images/f1-7.jpg" style="width:100%">
+      <img src="images/f1-8.jpg" style="width:100%">
+      <img src="images/f1-9.jpg" style="width:100%">
+      <img src="images/f1-10.jpg" style="width:100%">
+      <img src="images/f1-11.jpg" style="width:100%">
+
+      </div>
+    <!-- End photo grid -->
+    </div>
+  <!-- End Portfolio Section -->
+  </div>
+
+    <!-- About Section -->
+    <div class="w3-padding-64 w3-content" id="about">
+    <h2 class="w3-text-light-grey">More about me</h2>
+    <hr style="width:200px" class="w3-opacity">
+
+    <!-- Grid for photos -->
+    <div class="w3-row-padding" style="margin:0 -16px">
+      <div class="w3-half">
+        <img src="images/f1-13.jpg" style="width:100%">
+        <img src="images/f1-15.jpg" style="width:100%">
+        <img src="images/f1-17.jpg" style="width:100%">
+
+
+      </div>
+
+      <div class="w3-half">
+      <img src="images/f1-14.jpg" style="width:100%">
+      <img src="images/f1-16.jpg" style="width:100%">
+
+
+      </div>
+    <!-- End photo grid -->
+    </div>
+  <!-- End Portfolio Section -->
+  </div>
+
+  <!-- Contact Section -->
+  <div class="w3-padding-64 w3-content w3-text-grey" id="contact">
+    <h2 class="w3-text-light-grey">Contact Me</h2>
+    <hr style="width:200px" class="w3-opacity">
+
+    <div class="w3-section">
+      <p class="w3-white"><i class="fa fa-phone fa-fw w3-text-black w3-xxlarge w3-margin-right"></i> Mobile no: +8801711530206 <br>
+      <i class="fa fa-envelope fa-fw w3-text-black w3-xxlarge w3-margin-right"> </i> focusfurniturebangladesh@gmail.com <br></p>
+    </div><br>
+
+  <!-- End Contact Section -->
+  </div>
+
+  <!-- Leave a review -->
+  <div class="w3-light-grey w3-padding-large w3-padding-32 w3-margin-top" id="reviews">
+    <h3 class="w3-center">Leave a review</h3>
+    <hr>
+    <p>Tell us how you feel about our photographer's service, including what areas to improve upon</p>
+    
+    <form action="port_conn.php" method="post">
+    
+
+    <div class="w3-section">
+        <label>Photographer ID</label>
+        <input class="w3-input w3-padding-16" type="number" name="photographer_id" id="photographer_id" value="2">
+      </div>
+
+      <div class="w3-section">
+        <label>Name</label>
+        <input class="w3-input w3-padding-16" type="text" required name="name" id="name" value="<?php echo $full_name ?>">
+      </div>
+      <div class="w3-section">
+        <label>Email</label>
+        <input class="w3-input w3-padding-16" type="text" required name="email" id="email" value="<?php if(isset($_COOKIE["email"])) { echo $_COOKIE["email"]; } ?>">
+      </div>
+      <div class="w3-section">
+        <label>Rating</label>
+        <br>
+        <input type="radio" id="star" name="star" value="5">
+        <label for="html">&#11088; &#11088; &#11088; &#11088; &#11088;</label><br>
+         <input type="radio" id="star" name="star" value="4">
+         <label for="css">&#11088; &#11088; &#11088; &#11088;</label><br>
+         <input type="radio" id="star" name="star" value="3">
+         <label for="javascript">&#11088; &#11088; &#11088;</label><br>
+         <input type="radio" id="star" name="star" value="2">
+         <label for="javascript">&#11088; &#11088;</label><br>
+         <input type="radio" id="star" name="star" value="1">
+         <label for="javascript">&#11088;</label>
+
+      </div>
+      <div class="w3-section">
+        <label>Message</label>
+        <br>
+        <textarea  class="w3-input w3-padding-16" rows="10" columns="50" placeholder="Message" id="message" name="message"></textarea>
+      </div>
+      <button type="submit" class="w3-button w3-block w3-dark-grey">Submit</button>
+    </form><br>
+
+  
+
+
+
+  </div>
+
+    <!-- Reviews -->
+    <!-- <div class="w3-light-grey w3-padding-large w3-padding-32 w3-margin-top" id="reviews">
+    <h3 class="w3-center">Reviews</h3>
+    <hr>
+    <p>Tell us how you feel about our photographer's service, including what areas to improve upon</p>
+    
+    <form action="port_conn.php" method="post">
+    
+
+    <div class="w3-section">
+        <label>Photographer ID</label>
+        <input class="w3-input w3-padding-16" type="number" name="photographer_id" id="photographer_id" value="2">
+      </div>
+
+      <div class="w3-section">
+        <label>Name</label>
+        <input class="w3-input w3-padding-16" type="text" required name="name" id="name" value="<?php echo $full_name ?>">
+      </div>
+      <div class="w3-section">
+        <label>Email</label>
+        <input class="w3-input w3-padding-16" type="text" required name="email" id="email" value="<?php if(isset($_COOKIE["email"])) { echo $_COOKIE["email"]; } ?>">
+      </div>
+      <div class="w3-section">
+        <label>Message</label>
+        <br>
+        <textarea  class="w3-input w3-padding-16" rows="10" columns="50" placeholder="Message" id="message" name="message"></textarea>
+      </div>
+      <button type="submit" class="w3-button w3-block w3-dark-grey">Submit</button>
+    </form><br>
+
+  
+
+
+
+  </div> -->
+
+<!-- Reviews -->
+<div class="w3-light-grey w3-padding-large w3-padding-32 w3-margin-top" id="reviews">
+    <h3 class="w3-center">Reviews</h3>
+    <hr>
+    <p>This is what our previous customers had to say about the photographer:</p>
+    <br>
+    <br>
+    <br>
+    <br>
+
+
+    <?php
+// IMPORTING REVIEWS
+
+// Setting photographer ID
+$id=2;
+$count=0;
+// FETCHING NAME FROM DATABASE
+// connecting to database called fotoboo
+	$conn = mysqli_connect('localhost','root','','fotoboo');
+    if(!$conn){
+        echo 'Connection error' . mysqli.connect_error();
+    }
+    
+
+
+    //writing query for fetching everything from database
+    $sql = "SELECT * FROM reviews";
+
+
+    //making query and getting result
+    $db_email = mysqli_query($conn, $sql);
+
+
+    //fetching the resulting rows as array
+    $registration = mysqli_fetch_all($db_email, MYSQLI_ASSOC);
+    
+    //FETCHING NAME, BIO & CONTACT FROM PORTFOLIO
+    foreach(array_reverse($registration) as $reg){
+      //$int_value = intval( $string );
+        $check_id = intval(htmlspecialchars($reg['photographer_id']));
+
+ 
+        if($check_id==$id && $count!=10){
+            $namerev_fill=htmlspecialchars($reg['name']);
+            $review=htmlspecialchars($reg['message']);
+            $count++;
+
+            //echo '<input type="text" name="name1" value="'.$var.'">';
+
+            echo '<hr>';
+
+            echo "Name: \r\n";
+
+            echo '<br>';
+
+            echo '<br>';
+
+            echo '<td><b>'.$namerev_fill.'</b></td>';
+
+            echo "<br>";
+
+            echo "<br>";
+
+            echo "\r\n";
+
+            echo "Review: \r\n";
+
+            echo "<br>";
+
+            echo "<br>";
+
+            echo '<td><b>'.$review.'</b></td>';
+
+            echo "<br>";
+
+            echo "<br>";
+
+            echo "<br>";
+
+            echo "<br>";
+
+            echo "\r\n";
+
+            echo '<hr>';
+
+
+
+
+            
+
+           
+            
+        }
+        else{
+            echo "";
+        }
+    
+    }
+
+    
+
+
+?>
+
+
+
+  </div>
+  
+
+
+    <!-- Footer -->
+  <footer class="w3-content w3-padding-64 w3-text-grey w3-xlarge">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+    
+  <!-- End footer -->
+  </footer>
+
+<!-- END PAGE CONTENT -->
+</div>
+
+
+
+  <footer class="w3-center w3-black w3-padding-64 w3-opacity w3-hover-opacity-off" >
+  <a href="https://www.facebook.com/FocusDesignStudio2021" class="w3-button w3-hover-opacity"> <i class=" fa fa-facebook-official" style="font-size:36px"></i> Follow Us</a>
+
+  <div class="w3-xlarge w3-section button">
+    <i href="https://www.facebook.com/FocusDesignStudio2021" class="fa fa-facebook-official w3-hover-opacity button"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+
+  </div>
+
+  <!-- Footer -->
+<footer class="w3-center w3-black w3-padding-64 w3-opacity w3-hover-opacity-off" >
+  <a href="#" class="w3-button w3-light-grey"> <i class="fa fa-arrow-circle-up" style="font-size:36px"></i> To the top</a>
+
+  <p> POWERED BY TEAM FOCUS </p>
+  
+</footer>
+
+</body>
+</html>
